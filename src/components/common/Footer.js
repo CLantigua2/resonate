c8e55ccbb6b1db1920240fe9ec98d0fe02e4c91b
Carlos Lantigua <clantigua954@gmail.com>
2019-04-15 17:36:07 -0400

    adding toml file and base footer design

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,21 +1,51 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import styled from '@emotion/styled'
+import Logo from '../../image/logo.png'
 
 const Footer = () => (
     <Container>
         <footer>
-            <h4>Footer stuff goes here</h4>
+            <div>
+        <Link to="/"><img src={Logo} alt="Resonate logo" className="logo" /></Link>
+            </div>
+            <div>
+            <Link to="/">About Us</Link>
+            </div>
+            <div>
+            <Link to="/">Careers</Link>
+            </div>
+            <div>
+            <Link to="/">Blog</Link>
+            </div>
+            <div>
+            <Link to="/">Contact Us</Link>
+            </div>
         </footer>
     </Container>
 )
+
+
+
+
 
 export default Footer
 
 const Container = styled.div`
     height: 100px;
-    display: flex;
-    flex-direction: row;
     width: 100%;
     background: ${({theme}) => theme.color.secondary};
     margin-top: -100px;
+    padding: 25px;
+    footer {
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        align-items: center;
+            .logo {
+                margin: 0 0 30px 0;
+                width: 100px;
+            }
+        }
 `
