c1e2280b618d12b596449f491d9e809d8bd7a51c
Carlos Lantigua <clantigua954@gmail.com>
2019-04-17 18:54:44 -0400

    fixed sticky nav style and added animation fade

--- src/components/NavBarScroll.js
+++ src/components/NavBarScroll.js
@@ -2,18 +2,21 @@ import React from 'react'
 import styled from '@emotion/styled'
 import { NavLink } from 'react-router-dom'
 import useScrollPosition from './useScrollPosition'
+import Fade from 'react-reveal/Fade'
 
 const NavBarScroll = () => {
     const useScroll = useScrollPosition()
     if (useScroll > 130) {
         return (
             <StickyContainer>
+                <Fade>
                         <div className="sticky">
                             <NavLink to="/">Features</NavLink>
                             <NavLink to="/">Products</NavLink>
                             <NavLink to="/">Pages</NavLink>
                             <NavLink to="/">Shop</NavLink>
                         </div>
+                </Fade>
             </StickyContainer >
         )
     } else {
